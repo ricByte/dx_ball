ab05d5a490d34540a4ef95ac632ef97c82942c39
Riccardo Vecchi <riccardo.vecchi@lastminute.com>
2022-01-13 20:26:36 +0100

    Create Ball animation

--- components/Field/Field.tsx
+++ components/Field/Field.tsx
@@ -1,22 +1,46 @@
 import styles from "./Field.module.scss";
 import {Block} from "../Block/Block";
-import {Ball, BallPosition} from "../Ball/Ball";
+import {Ball} from "../Ball/Ball";
+import {useEffect, useRef, useState} from "react";
 
 export const Field = function Home() {
     let blockWidth = 50;
     let widthField = 800;
+    let heightField = 600;
+    const FPS = 100;
+    const speed = 10;
+    const ballRadius = 10;
+
     const blockPerRow: number = widthField / blockWidth
-    let position: BallPosition = {
-        left: widthField / 2,
-        bottom: 0
-    };
+
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    const timer = useRef(null);
+
+    useEffect(() => {
+        if ((x < widthField - ballRadius) && (y < heightField - ballRadius * 2 - 2))
+            timer.current = setInterval(() => {
+                setX((current) => current + speed);
+                setY((current) => current + speed);
+            }, FPS);
+        else
+            clearInterval(timer.current)
+
+        // clear on component unmount
+        return () => {
+            clearInterval(timer.current);
+        };
+    }, [x]);
 
     return (
-        <div className={styles.fieldContainer}>
-            {[...Array(blockPerRow)].map((value, index) =>
-                <Block/>
-            )}
-            <Ball position={position}/>
+        <div>
+            <div className={styles.fieldContainer}>
+                {[...Array(blockPerRow)].map((value, index) =>
+                    <Block/>
+                )}
+                <Ball position={{left: x, bottom: y}}/>
+            </div>
+            {`x:${x};y:${y}`}
         </div>
-    )
+    );
 }
