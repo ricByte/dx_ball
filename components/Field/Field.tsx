54df930a2cedbc28d8fc6c825c4dd18d0e5b1eb5
Riccardo Vecchi <riccardo.vecchi@lastminute.com>
2022-01-13 19:10:31 +0100

    Create Row

--- components/Field/Field.tsx
+++ components/Field/Field.tsx
@@ -2,9 +2,14 @@ import styles from "./Field.module.scss";
 import {Block} from "../Block/Block";
 
 export const Field = function Home() {
+    let blockWidth = 50;
+    let widthField = 800;
+    const blockPerRow: number = widthField / blockWidth
     return (
         <div className={styles.fieldContainer}>
-            <Block/>
+            {[...Array(blockPerRow)].map((value, index) =>
+                <Block/>
+            )}
         </div>
     )
 }
